2d95cf48c01c7ab75a58de8ee511301ee5520372
Esubalew Chekol <esubalewchekol6@gmail.com>
2024-01-10 19:35:59 +0300

    Update urls.py

--- evaluator/urls.py
+++ evaluator/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('evaluation/', include('evaluation.urls', namespace='evaluation')),
+    path('', RedirectView.as_view(url='/evaluation/', permanent=True), name='index_redirect'),
 ]
