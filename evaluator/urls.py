dd011805a1862089169730225178be1986453b5a
Esubaalew <esubalewchekol6@gmail.com>
2024-01-11 11:47:38 +0300

    url changes

--- evaluator/urls.py
+++ evaluator/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
@@ -23,3 +25,5 @@ urlpatterns = [
     path('evaluation/', include('evaluation.urls', namespace='evaluation')),
     path('', RedirectView.as_view(url='/evaluation/', permanent=True), name='index_redirect'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
